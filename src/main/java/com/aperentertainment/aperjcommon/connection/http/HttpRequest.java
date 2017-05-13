5f3235a4d3d060be9a53856544cfdc9620235f73
wAperClem <waperclem@gmail.com>
2017-05-13 10:20:04 +0200

    Updating license

--- src/main/java/com/aperentertainment/aperjcommon/connection/http/HttpRequest.java
+++ src/main/java/com/aperentertainment/aperjcommon/connection/http/HttpRequest.java
@@ -1,20 +1,10 @@
 /*
- * Copyright © 2017 AperEntertainment
+ * Copyright © 2017 AperEntertainment <aper.entertainment@gmail.com>
  *
  * This file is part of aperjcommon.
  *
- * aperjcommon is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * aperjcommon is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with aperjcommon.  If not, see <http://www.gnu.org/licenses/>.
+ * Licensed under the MIT license. For more information,
+ * see the LICENSE file.
  */
 
 package com.aperentertainment.aperjcommon.connection.http;
